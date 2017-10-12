12b3b35d5930023fb05f991e96afebb8741979e7
Yosuke Kabuto <bluewhale1982@gmail.com>
2017-10-13 08:49:56 +0900

    Loop to execute the same tests but different classes

--- test/dazai_test.rb
+++ test/dazai_test.rb
@@ -2,106 +2,65 @@ require 'faker'
 require 'test_helper'
 
 class DazaiTest < Minitest::Test
-  def test_that_it_has_a_version_number
-    assert ::Dazai::VERSION
-  end
+  [::Dazai, Faker::Dazai].each do |klass|
+    @@klass = klass
 
-  def test_it_has_default_book
-    assert ::Dazai::DEFAULT_BOOK
-  end
+    def test_that_it_has_a_version_number
+      assert ::Dazai::VERSION
+    end
 
-  def test_it_has_book_path
-    subject = ::Dazai::BOOKS_PATH
+    def test_it_has_default_book
+      assert @@klass::DEFAULT_BOOK
+    end
 
-    assert subject.exist?
-    assert subject.directory?
-  end
+    def test_it_has_book_path
+      subject = @@klass::BOOKS_PATH
 
-  def test_sentence
-    subject = ::Dazai.sentence
+      assert subject.exist?
+      assert subject.directory?
+    end
 
-    assert_kind_of String, subject
-    refute_match(/\n/,     subject)
-  end
+    def test_sentence
+      subject = @@klass.sentence
 
-  def test_paragraph
-    subject = ::Dazai.paragraph
+      assert_kind_of String, subject
+      refute_match(/\n/,     subject)
+    end
 
-    assert_kind_of String, subject
-    assert_match(/\n/,     subject)
-  end
+    def test_paragraph
+      subject = @@klass.paragraph
 
-  def test_paragraph_with_title
-    subject = ::Dazai.paragraph(title: :romanesque)
+      assert_kind_of String, subject
+      assert_match(/\n/,     subject)
+    end
 
-    assert_kind_of String, subject
-    assert_match(/\n/,     subject)
-  end
+    def test_paragraph_with_title
+      subject = @@klass.paragraph(title: :romanesque)
 
-  def test_paragraphs
-    subject = ::Dazai.paragraphs
+      assert_kind_of String, subject
+      assert_match(/\n/,     subject)
+    end
 
-    assert_kind_of String, subject
-    assert_match(/\n\n/,   subject)
-  end
+    def test_paragraphs
+      subject = @@klass.paragraphs
 
-  def test_paragraphs_with_title
-    subject = ::Dazai.paragraphs(title: :romanesque)
+      assert_kind_of String, subject
+      assert_match(/\n\n/,   subject)
+    end
 
-    assert_kind_of String, subject
-    assert_match(/\n\n/,   subject)
-  end
+    def test_paragraphs_with_title
+      subject = @@klass.paragraphs(title: :romanesque)
 
-  def test_paragraphs_with_count
-    subject = ::Dazai.paragraphs(count: 5)
+      assert_kind_of String, subject
+      assert_match(/\n\n/,   subject)
+    end
 
-    assert_kind_of String, subject
-    assert_match(/\n\n/,   subject)
-    assert_equal 5,        subject.split("\n\n").count
-  end
-end
+    def test_paragraphs_with_count
+      subject = @@klass.paragraphs(count: 5)
 
-class ::Faker::DazaiTest < Minitest::Test
-  def test_sentence
-    subject = Faker::Dazai.sentence
-
-    assert_kind_of String, subject
-    refute_match(/\n/,     subject)
-  end
-
-  def test_paragraph
-    subject = Faker::Dazai.paragraph
-
-    assert_kind_of String, subject
-    assert_match(/\n/,     subject)
-  end
-
-  def test_paragraph_with_title
-    subject = Faker::Dazai.paragraph(title: :romanesque)
-
-    assert_kind_of String, subject
-    assert_match(/\n/,     subject)
-  end
-
-  def test_paragraphs
-    subject = Faker::Dazai.paragraphs
-
-    assert_kind_of String, subject
-    assert_match(/\n\n/,   subject)
-  end
-
-  def test_paragraphs_with_title
-    subject = Faker::Dazai.paragraphs(title: :romanesque)
-
-    assert_kind_of String, subject
-    assert_match(/\n\n/,   subject)
-  end
-
-  def test_paragraphs_with_count
-    subject = Faker::Dazai.paragraphs(count: 5)
-
-    assert_kind_of String, subject
-    assert_match(/\n\n/,   subject)
-    assert_equal 5,        subject.split("\n\n").count
+      assert_kind_of String, subject
+      assert_match(/\n\n/,   subject)
+      assert_equal 5,        subject.split("\n\n").count
+    end
   end
 end
